1a320ab0ce5d80820d742f32ba3553ce467260bf
sasiyatechnic0 <sasitechnic0@gmail.com>
2025-04-20 15:30:10 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "session id",
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=s4Rl3LCR#_yrWzojdy0Cb9X9o2ZSEmucEPJhApwm_e-PjgIYQjv0",
 PREFIX: process.env.PREFIX || '.' ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
